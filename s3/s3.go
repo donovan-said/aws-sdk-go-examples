1661110cbcaf4869dade3ebd3d4e181b49cf3381
Donovan Said <donovan.s.said@gmail.com>
2024-02-16 16:46:26 +0000

    Formatting.

--- s3/s3.go
+++ s3/s3.go
@@ -15,32 +15,32 @@ import (
 )
 
 func create_s3_bucket() {
-    // Load the shared AWS Configuration (~/.aws/config)
+	// Load the shared AWS Configuration (~/.aws/config)
 
-    cfg, err := config.LoadDefaultConfig(context.TODO())
-    if err != nil {
-        log.Fatal(err)
-    }
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Create S3 service client
-    client := s3.NewFromConfig(cfg)
+	// Create S3 service client
+	client := s3.NewFromConfig(cfg)
 
-    // Get the first page of results from ListObjectV2 for an S3 bucket
-    output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-        Bucket: aws.String("MySampleBucket"),
-    })
-    if err != nil {
-        log.Fatal()
-    }
+	// Get the first page of results from ListObjectV2 for an S3 bucket
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("MySampleBucket"),
+	})
+	if err != nil {
+		log.Fatal()
+	}
 
-    log.Println("Results from first page: ")
-    for _, object := range output.Contents {
-        log.Printf("key=%s, size=%d", aws.ToString(object.Key), *object.Size)
-    }
+	log.Println("Results from first page: ")
+	for _, object := range output.Contents {
+		log.Printf("key=%s, size=%d", aws.ToString(object.Key), *object.Size)
+	}
 
 }
 
 func main() {
-    // Init function
-    create_s3_bucket()
+	// Init function
+	create_s3_bucket()
 }
